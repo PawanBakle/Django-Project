9ccf7ad6f23271caaa87ce0b609f7861a5151735
pawankumar bakle <PawanBakle>
2022-12-15 19:40:08 +0530

    Django templates

--- settings.py
+++ settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     #third party apps
 
     #own
-    'pages',
     'products',
+    
+    'pages',
     
 ]
 
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- urls.py
+++ urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view,contact_view
-
+from pages.views import home_view,contact_view,about_view,payment_view
+from products.views import product_detail_view
 
 urlpatterns = [
-    path('',home_view,name = 'home'),
-    path('home/',home_view,name = 'home'),
-    path('contact/',contact_view),
     path('admin/', admin.site.urls),
+    path('',home_view,name = 'home'),
+    #path('home/',home_view,name = 'home'),
+    path('contact/',contact_view),
+    
+    path('about/', about_view),
+    path('product/', product_detail_view),
+    path('payment/', payment_view),
+   
 ]
