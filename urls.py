e4d4778ccf1cfa182a065d2c615e3d9e4e7c4964
pawankumar bakle <PawanBakle>
2022-12-16 18:43:05 +0530

    Forms and validation

--- urls.py
+++ urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,contact_view,about_view,payment_view
-from products.views import product_detail_view
+from products.views import product_detail_view,product_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +25,7 @@ urlpatterns = [
     path('contact/',contact_view),
     
     path('about/', about_view),
+    path('create/', product_create_view),
     path('product/', product_detail_view),
     path('payment/', payment_view),
    
